3c383a3dc0d497be6fe96cb2bfe684faa655b953
macctown <macctown@hotmail.com>
2016-05-08 21:18:36 -0400

    init commit - product and search service complete

---
+++ demo.js
@@ -0,0 +1,39 @@
+//Product Service
+//http://ssg-api.shop.com/stores/v1/products/874694776?allperms=false&siteId=123
+//prodID, allperms, siteId
+
+
+//Search Service
+//http://ssg-api.shop.com/sites/v1/search/term/socks?page=1&count=1
+//term, page, count
+
+
+//APN categories
+//https://api.shop.com:8443/AffiliatePublisherNetwork/v1/categories?publisherID=TEST&locale=en_US
+//publisherID, locale
+
+
+//APN Products
+//https://api.shop.com:8443/AffiliatePublisherNetwork/v1/products?publisherID=TEST&locale=en_US&perPage=15
+//publisherID, locale, start, perPage, term. categoryId, brandId, sellerId, priceRangeId
+//https://api.shop.com:8443/AffiliatePublisherNetwork/v1/products/834207132?publisherID=TEST&locale=en_US
+//productID, publisherID, locale
+
+
+//APN Tax and Shipping
+//https://api.shop.com:8443/AffiliatePublisherNetwork/v1/taxandshipping?prodIds=123
+//prodIds, quantity, state, zip, city, country, street
+
+
+
+var Shop = require('node-shop.com').initShop({
+	apikey: 'YOUR_API_KEY'
+});
+
+Shop.products(874694776, {allperms: false})
+.then(function (data) {
+  console.log(data);
+})
+.catch(function (err) {
+  console.error(err);
+});
